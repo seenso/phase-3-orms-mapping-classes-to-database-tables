c737b0849a3f51982d43f385a950e6ad9990e7fb
seenso <>
2021-12-16 21:37:06 -0900

    commit tree 2605e2a8c3a39aec68c200e22a206aac8aed9200

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,6 @@ Bundler.require
 require_relative '../lib/song'
 
 DB = { conn: SQLite3::Database.new("db/music.db") }
+# ^ is equal to a hash that contains our connection to the database.
+# We can access this DB constant and the db connection it holds like:
+#       DB[:conn]
--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,41 @@
 class Song
 
-  attr_accessor :name, :album
+  attr_accessor :name, :album, :id
 
-  def initialize(name:, album:)
+  def initialize(name:, album:, id: nil)
+    @id = id
     @name = name
     @album = album
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+    SQL
+    DB[:conn].execute(sql) #accessing the DB constant in environment.rb and accessing the connection to our db
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
+    SQL
+    #add song to database
+    DB[:conn].execute(sql, self.name, self.album)
+    #get song from database and save it to the Ruby instance
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+
+    #return Ruby instance
+    self
+  end
+
+  def self.create(name:, album:)
+    song = Song.new(name: name, album: album)
+    song.save
   end
 
 end
